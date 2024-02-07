9ac2b95defacad26350918fa83ca4f133ad04e66
Jack Beer <26jbeer@shcp.edu>
2024-02-07 14:56:23 -0800

    Daemon entry point added

---
+++ src/main/java/org/manager/Daemon.java
@@ -0,0 +1,9 @@
+package org.manager;
+
+public class Daemon {
+
+   public static void main(String[] args) {
+      System.out.println("Starting Daemon!");
+   }
+
+}
--- src/main/java/org/manager/Main.java
+++ src/main/java/org/manager/Main.java
@@ -1,7 +1,8 @@
-package org.main;
+package org.manager;
 
 public class Main {
+
    public static void main(String[] args) {
-      System.out.println("Hello world!");
+      System.out.println("Starting Main!");
    }
 }
