8a59f84fb837d10c2a21a5f41efda364c30ea576
Jack Beer <26jbeer@shcp.edu>
2024-02-08 18:16:29 -0800

    no more yapping

--- src/main/java/org/manager/Daemon.java
+++ src/main/java/org/manager/Daemon.java
@@ -5,18 +5,16 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
-/* The Daemon class runs background activities to maintain an accurate assignment listing.
-Daemon.main utilizes org.manager.Clock to track elapsed time and periodically execute methods critical to program maintenance. */
 public class Daemon {
 
-   private static final Clock clock = new Clock(); //Clock object manages program-relative time
-   private static final Console con = new Console(Daemon.class.getName()); //Console object manages Daemon specific outputs and errors
+   private static final Clock clock = new Clock(); 
+   private static final Console con = new Console(Daemon.class.getName());
    private static final ScheduledExecutorService clockExecutor = Executors.newScheduledThreadPool(1); //delay executor
    private static final Runnable clockUpdate = clock::update; //delay executor method runner
 
    public static void daemon() {
       con.out("NotionManager daemon started at " + clock);
-      clockExecutor.scheduleAtFixedRate(clockUpdate, 0, 1, TimeUnit.SECONDS); //call delay executor with delay period 1 second
+      clockExecutor.scheduleAtFixedRate(clockUpdate, 0, 1, TimeUnit.SECONDS);
       con.setLevel(3);
 
       instantiate();
